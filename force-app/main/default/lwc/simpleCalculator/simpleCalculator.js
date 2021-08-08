e58d2d216aa7c2dc7a2c225d1e07c8d4b18e048f
Ahad Hossain <hossain1814@gmail.com>
2021-08-08 20:24:24 +0600

    S-4.added all handler to html.bind numberChangeHandler and addHandler to JS file.

--- force-app/main/default/lwc/simpleCalculator/simpleCalculator.js
+++ force-app/main/default/lwc/simpleCalculator/simpleCalculator.js
@@ -1,3 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class SimpleCalculator extends LightningElement {}
+export default class SimpleCalculator extends LightningElement {
+  @track currentResult;
+
+  firstNumber;
+  secondNumber;
+
+  numberChangeHandler(event) {
+    const inputBoxName = event.target.name;
+    if(inputBoxName === 'firstNumber') {
+      this.firstNumber = event.target.value;
+    } else if(inputBoxName === 'secondNumber') {
+      this.secondNumber = event.target.value;
+    }
+  }
+
+  addHandler() {
+    const firstN = parseInt(this.firstNumber);
+    const secondN = parseInt(this.secondNumber);
+
+    this.currentResult = `Result of ${this.firstNumber} + ${this.secondNumber} is ${firstN + secondN}`
+  }
+}
