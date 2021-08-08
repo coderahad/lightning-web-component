b04b2c1268741df577e682eebced04bbd7faec32
Ahad Hossain <hossain1814@gmail.com>
2021-08-08 14:35:24 +0600

    S-4.using for:each iterator to iterate.

--- force-app/main/default/lwc/conditionalRenderingExample/conditionalRenderingExample.js
+++ force-app/main/default/lwc/conditionalRenderingExample/conditionalRenderingExample.js
@@ -3,6 +3,7 @@ import { LightningElement, track } from 'lwc';
 export default class ConditionalRenderingExample extends LightningElement {
   @track displayDiv = false;
 
+  @track cityList = ['Dhaka', 'New York', 'Dubai', 'Montreal'];
   showDivHandler(event) {
     this.displayDiv = event.target.checked;
   }
