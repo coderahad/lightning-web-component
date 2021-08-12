7d9869da1e4e6d11a27037b87d03a40e1c8cf3c7
Ahad Hossain <hossain1814@gmail.com>
2021-08-12 14:12:39 +0600

    S-8.Dispatching and Handling Events.create a custom event on clicking.dispatch it for parent cmp.

--- force-app/main/default/lwc/meetingRoom/meetingRoom.js
+++ force-app/main/default/lwc/meetingRoom/meetingRoom.js
@@ -1,7 +1,16 @@
 import { LightningElement, api } from 'lwc';
 
 export default class MeetingRoom extends LightningElement {
-  // @api meetingRoomInfo = {roomName: 'A-01', roomCapacity: '12'};
+  @api meetingRoomInfo = {roomName: 'A-01', roomCapacity: '12'};
 
   @api showRoomInfo = false;
+
+  tileClickedHandler() {
+    // crate event with CustomEvent Constructor and input payload.
+    // When using this event we need to prepend on: ontileclick.
+    const tileClicked = new CustomEvent('tileclick', {detail: this.meetingRoomInfo });
+
+    //fire the event
+    this.dispatchEvent(tileClicked);
+  }
 }
