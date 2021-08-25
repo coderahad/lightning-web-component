59e9468a7b7a305c8a71c376218d379dcd61c58b
Ahad Hossain <hossain1814@gmail.com>
2021-08-26 02:07:27 +0600

    S-9.import and use createRecord module for creating new record.

--- force-app/main/default/lwc/accountManagerLDS/accountManagerLDS.js
+++ force-app/main/default/lwc/accountManagerLDS/accountManagerLDS.js
@@ -1,3 +1,27 @@
 import { LightningElement } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
 
-export default class AccountManagerLDS extends LightningElement {}
+export default class AccountManagerLDS extends LightningElement {
+    accountName;
+    accountPhone;
+    accountWebsite;
+
+    accountNameChangeHandler(event) {
+      this.accountName = event.target.value;
+    }
+
+    accountPhoneChangeHandler(event) {
+      this.accountName = event.target.value;
+    }
+
+    accountWebsiteChangeHandler(event) {
+      this.accountName = event.target.value;
+    }
+
+    createAccount() {
+      const fields = {'Name': this.accountName, 'Phone': this.accountPhone, 'Website': this.accountWebsite };
+      const recordInput = { apiName: 'Account', fields };
+
+      createRecord(recordInput)
+    }
+}
