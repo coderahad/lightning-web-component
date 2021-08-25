724839fb0e8eeeda102c0ef9287418eec5814175
Ahad Hossain <hossain1814@gmail.com>
2021-08-25 13:07:54 +0600

    S-8.register events during connectedCallback component lifecycle. completed.

--- force-app/main/default/lwc/selectedMeetingRoom/selectedMeetingRoom.js
+++ force-app/main/default/lwc/selectedMeetingRoom/selectedMeetingRoom.js
@@ -1,14 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import { registerListener, unregisterAllListeners } from 'c/pubsub';
 import { CurrentPageReference } from 'lightning/navigation';
 
 export default class SelectedMeetingRoom extends LightningElement {
-    selectedMeetingRoom;
+    // error happens when I didn't assign this blank object. because it was null
+    // and component could not read the roomName intially.
+    selectedMeetingRoom = {};
+    // 'pageRef' and 'pageReference' from meetingRoom are examined by pubsub if they are in same page.
+    @wire(CurrentPageReference) pageRef;
+
+    connectedCallback() {
+      registerListener('pubsubtileclick', this.onMeetingRoomSelectHandler, this);
+    }
 
     disconnectedCallback(){
       // unregisterAllListener() takes an object. 'this' is the current object.
       unregisterAllListeners(this);
     }
-
-    
+    //this is the callback of registerListener. payload we get from the fired event
+    onMeetingRoomSelectHandler(payload) {
+      this.selectedMeetingRoom = payload;
+    }
 }
