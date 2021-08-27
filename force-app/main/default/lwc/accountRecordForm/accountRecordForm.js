21b86661ebaf4456581f7e8cb9d8c8dcf07953b0
Ahad Hossain <hossain1814@gmail.com>
2021-08-27 20:07:26 +0600

    S-9.using layout-type in record-form to render all fields in a layout.

--- force-app/main/default/lwc/accountRecordForm/accountRecordForm.js
+++ force-app/main/default/lwc/accountRecordForm/accountRecordForm.js
@@ -1,12 +1,7 @@
 import { LightningElement } from 'lwc';
-import NAME_FIELD from '@salesforce/schema/Account.Name';
-import PHN_FIELD from '@salesforce/schema/Account.Phone';
-import WEBSITE_FIELD from '@salesforce/schema/Account.Website';
 
 export default class AccountRecordForm extends LightningElement {
   recordId;
-
-  fieldsArray = [NAME_FIELD, PHN_FIELD, WEBSITE_FIELD];
 
   handleSuccess(event) {
     this.recordId = event.detail.id;
