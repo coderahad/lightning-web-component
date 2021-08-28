76c698e59b1c4e1e53d00e5863f3e5cf5774ffbf
Ahad Hossain <hossain1814@gmail.com>
2021-08-28 21:15:34 +0600

    S-9.completed.call apex method imperatively.

--- force-app/main/default/lwc/accountManagerApex/accountManagerApex.js
+++ force-app/main/default/lwc/accountManagerApex/accountManagerApex.js
@@ -1,14 +1,27 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement } from 'lwc';
 import getAllAccounts from '@salesforce/apex/AccountManager.getAccounts';
 
 export default class AccountManagerApex extends LightningElement {
 
-  @wire(getAllAccounts) accounts;
+  numberOfRecords;
+  accounts;
 
   get responseReceived(){
     if(this.accounts) {
       return true;
     }
     return false;
+  }
+
+  numOfAccountChangeHandler(event) {
+    this.numberOfRecords = event.target.value;
+  }
+
+  getAccounts(){
+    getAllAccounts({numberOfRecords: this.numberOfRecords }).then(response => {
+      this.accounts = response;
+    }).catch(error => {
+      console.error('Error in getting the accounts', error.body.message);
+    });
   }
 }
