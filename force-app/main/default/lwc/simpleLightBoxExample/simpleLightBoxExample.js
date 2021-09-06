7277e7be0d92fbf89df41495aae5838c303fd54b
Ahad Hossain <hossain1814@gmail.com>
2021-09-06 13:12:13 +0600

    S-10.use renderedCallback lifecycle hook.since loadStyle and loadScript are promises we use them in Promise array.

--- force-app/main/default/lwc/simpleLightBoxExample/simpleLightBoxExample.js
+++ force-app/main/default/lwc/simpleLightBoxExample/simpleLightBoxExample.js
@@ -3,4 +3,16 @@ import SL from '@salesforce/resourceUrl/simplelightbox';
 // resource can have multiple js or css files.these functions are useful to load those files in our LWC.
 import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
 
-export default class SimpleLightBoxExample extends LightningElement {}
+export default class SimpleLightBoxExample extends LightningElement {
+
+    renderedCallback(){
+      Promise.all([
+        loadStyle(this, `${SL}/simpleLightbox-master/dist/simpleLightbox.css`),
+        loadScript(this, `${SL}/simpleLightbox-master/dist/simpleLightbox.js`)
+      ]).then(()=>{
+
+      }).catch((error) =>{
+        console.error('Could not initialize light box -', error)
+      })
+    }
+}
