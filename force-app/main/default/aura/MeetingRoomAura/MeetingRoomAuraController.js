eb756c9561674bee279f91e5c94ee3a7b5e69b9d
Ahad Hossain <hossain1814@gmail.com>
2021-09-29 12:14:33 +0600

    S-11.Aura and LWC interoperability.Communicate in the same cmp hierarchy.Create a MeetingRoomAura cmp. rewrite meetingRooms template in aura.intialize meetingRoomInfo with an aura handler init event.

---
+++ force-app/main/default/aura/MeetingRoomAura/MeetingRoomAuraController.js
@@ -0,0 +1,13 @@
+({
+    doInit : function(component, event, helper) {
+        component.set("v.meetingRoomInfo", [
+            {roomName: 'A-01', roomCapacity: '5'},
+            {roomName: 'A-02', roomCapacity: '6'},
+            {roomName: 'A-03', roomCapacity: '8'},
+            {roomName: 'B-01', roomCapacity: '10'},
+            {roomName: 'B-02', roomCapacity: '14'},
+            {roomName: 'B-03', roomCapacity: '15'},
+            {roomName: 'C-01', roomCapacity: '20'},
+          ]);
+    }
+})
