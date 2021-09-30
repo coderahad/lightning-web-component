b447d0305a29fc7720f35d981560b86b9c05ea02
Ahad Hossain <hossain1814@gmail.com>
2021-09-30 09:29:04 +0600

    S-12.Writing Test for your lwc.Each of the test method starts with 'it'

--- force-app/main/default/lwc/meetingRooms/__tests__/meetingRooms.test.js
+++ force-app/main/default/lwc/meetingRooms/__tests__/meetingRooms.test.js
@@ -2,5 +2,8 @@ import {createElement} from "lwc";
 import MeetingRooms from "c/c/meetingRooms";
 
 describe("c-meetingRooms", ()=>{
-
+  
+  it("count of meeting room should be 7", () => {
+    const meetingRooms = createElement("c-meetingRooms", {is:MeetingRooms});
+  })
 });
