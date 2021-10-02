5a7059d540d4e3fc1410de33936921628d9d9d05
Ahad Hossain <hossain1814@gmail.com>
2021-10-02 20:55:01 +0600

    S-13.cmt 1.create carSerchForm cmp markeup. need a apex class to query data.

---
+++ force-app/main/default/lwc/carSearchForm/carSearchForm.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class CarSearchForm extends LightningElement {
+  carTypes;
+
+  handleCarTypeChange(event) {
+
+  }
+
+  createNewCarType() {
+    
+  }
+
+} 
