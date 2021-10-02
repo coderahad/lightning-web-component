39abef7b635301c9d8315e7b1ce3ba93c51cefd8
Ahad Hossain <hossain1814@gmail.com>
2021-10-02 21:17:58 +0600

    S-13.cmt 2. create a class to get all record.fetch from the js and process it with @wire decorator.

--- force-app/main/default/lwc/carSearchForm/carSearchForm.js
+++ force-app/main/default/lwc/carSearchForm/carSearchForm.js
@@ -1,8 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getCarTypes from '@salesforce/apex/CarSearchFormController.getCarTypes';
 
 export default class CarSearchForm extends LightningElement {
   carTypes;
 
+  @wire(getCarTypes) 
+  wiredCarType({data, error}) {
+    if(data) {
+      this.carTypes = [{value:'', label: 'All Types'}];
+      data.forEach(element => {
+        const carType = {};
+        carType.label = element.Name;
+        carType.value = element.id;
+        this.carTypes.push(carType);
+      })
+    }
+  }
+  
   handleCarTypeChange(event) {
 
   }
