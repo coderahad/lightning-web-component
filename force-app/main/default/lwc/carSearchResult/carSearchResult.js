0d6f68dbc9b1f6ee188d656046753b58416859d3
Ahad Hossain <hossain1814@gmail.com>
2021-10-04 12:52:18 +0600

    S-13.cmt 9. Develop CarSearchResult cmp.Create a class for related cars of car type.

--- force-app/main/default/lwc/carSearchResult/carSearchResult.js
+++ force-app/main/default/lwc/carSearchResult/carSearchResult.js
@@ -1,5 +1,25 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getCars from '@salesforce/apex/CarSearchResultController.getCars';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class CarSearchResult extends LightningElement {
   @api carTypeId;
+  cars;
+
+  @wire(getCars, {carTypeId : this.carTypeId}) wiredCars({data, error}) {
+    if(data) {
+      this.cars = data;
+    } else if(error) {
+      this.showToast('ERROR', error.body.message, 'error');
+    }
+  }
+  showToast(title, message, variant) {
+    const evt = ShowToastEvent({
+      title: title,
+      message: message,
+      variant: variant,
+    });
+
+    this.dispatchEvent(evt);
+  }
 }
