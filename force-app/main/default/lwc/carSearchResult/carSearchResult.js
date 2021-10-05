9097b40f714953c813a630f1d534d7609d7ebdd3
Ahad Hossain <hossain1814@gmail.com>
2021-10-05 23:24:34 +0600

    S-13.cmt 14. a border around selected car when a user click a car.

--- force-app/main/default/lwc/carSearchResult/carSearchResult.js
+++ force-app/main/default/lwc/carSearchResult/carSearchResult.js
@@ -1,36 +1,41 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import getCars from '@salesforce/apex/CarSearchResultController.getCars';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
+
 export default class CarSearchResult extends LightningElement {
-  @api carTypeId;
-  cars;
-    // with $, wire adaptor become reactive. without $, it is static
-  @wire(getCars, {carTypeId : '$carTypeId'}) wiredCars({data, error}) {
-    if(data) {
-      this.cars = data;
-    } else if(error) {
-      this.showToast('ERROR', error.body.message, 'error');
-    }
-  }
-  showToast(title, message, variant) {
-    const evt = ShowToastEvent({
-      title: title,
-      message: message,
-      variant: variant,
-    });
+    @api carTypeId;
 
-    this.dispatchEvent(evt);
-  }
-  carSelectHandler(event){
-    const carId = event.detail;
-  }
+    @track cars;
+    @track selectedCarId;
 
-  // Here getCarsFound() was the bug. I followed the aura cmp getter method convention and component return 'No cars found'. 
-  get carsFound() {
-    if(this.cars){
-      return true;
+    @wire(getCars, {carTypeId : '$carTypeId'})
+    wiredCars({data, error}){
+        if(data){
+            this.cars = data;
+        } else if(error){
+            this.showToast('ERROR', error.body.message, 'error');
+        }
     }
-    return false;
-  }
+
+    showToast(title, message, variant) {
+        const evt = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+        });
+        this.dispatchEvent(evt);
+    }
+
+    carSelectHandler(event){
+        const carId = event.detail;
+        this.selectedCarId = carId;
+    }
+
+    get carsFound(){
+        if(this.cars){
+            return true;
+        }
+        return false;
+    }
 }
--- force-app/main/default/lwc/carTile/carTile.js
+++ force-app/main/default/lwc/carTile/carTile.js
@@ -1,14 +1,22 @@
 import { LightningElement, api } from 'lwc';
 
 export default class CarTile extends LightningElement {
-  @api car;
+    @api car;
+    @api carSelectedId;
 
-  handleCarSelect(event){
-    event.preventDefault();
+    handleCarSelect(event){
+        event.preventDefault();
 
-    const carId = this.car.Id;
+        const carId = this.car.Id;
 
-    const carSelect = new CustomEvent('carSelect', {detail: carId});
-    this.dispatchEvent(carSelect);
-  }
+        const carSelect = new CustomEvent('carselect', {detail:carId});
+        this.dispatchEvent(carSelect);
+    }
+
+    get isCarSelected(){
+        if(this.car.Id === this.carSelectedId){
+            return "tile selected";
+        }
+        return "tile";
+    }
 }
