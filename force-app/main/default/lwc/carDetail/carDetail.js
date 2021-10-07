f670b209637840e34851b416acc5322b8ce1ddde
Ahad Hossain <hossain1814@gmail.com>
2021-10-07 20:59:30 +0600

    S-13.cmt 20. render fields value in cmp. render cmp in carDetails cmp.

--- force-app/main/default/lwc/carDetail/carDetail.js
+++ force-app/main/default/lwc/carDetail/carDetail.js
@@ -15,4 +15,60 @@ export default class CarDetail extends NavigationMixin(LightningElement) {
           }
         });
     }
+
+    get carName(){
+      try{
+          return this.car.data.fields.Name.value;
+      }catch(error){
+          return 'NA'
+      } 
+    }
+
+    get ownerName(){
+      try{
+        return this.car.data.fields.Contact__r.value.fields.Name.value;
+      } catch(error){
+        return 'NA';
+      }
+    }
+  
+    get type(){
+      try{
+        return this.car.data.fields.Car_Type__r.value.fields.Name.value;
+      } catch(error){
+        return 'NA';
+      }
+    }
+  
+    get buildYear(){
+      try{
+        return this.car.data.fields.Build_Year__c.value;
+      } catch(error){
+        return 'NA';
+      }
+    }
+  
+    get perDayRent(){
+      try{
+        return this.car.data.fields.Per_Day_Rent__c.value;
+      } catch(error){
+        return 'NA';
+      }
+    }
+  
+    get mileage(){
+      try{
+        return this.car.data.fields.Mileage__c.value;
+      } catch(error){
+        return 'NA';
+      }
+    }
+  
+    get pictureUrl(){
+      try{
+        return this.car.data.fields.Picture__c.value;
+      } catch(error){
+        return 'NA';
+      }
+    }
 }
