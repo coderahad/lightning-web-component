940f4c6f7c7d0404fa1161c5615e2b498069f60d
Ahad Hossain <hossain1814@gmail.com>
2021-10-11 18:20:29 +0600

    S-13.cmt 22. create record in sf and show message.

--- force-app/main/default/lwc/addCarExperience/addCarExperience.js
+++ force-app/main/default/lwc/addCarExperience/addCarExperience.js
@@ -1,6 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import NAME_FIELD from '@salesforce/schema/Car_Experience__c.Name';
+import EXPERIENCE_FIELD from '@salesforce/schema/Car_Experience__c.Experience__c';
+import CAR_FIELD from '@salesforce/schema/Car_Experience__c.Car__c';
+import EXPERIENCE_OBJECT from '@salesforce/schema/Car_Experience__c'
 
 export default class AddCarExperience extends LightningElement {
+
+    @api carId;
 
     expTitle = '';
     expDescription = '';
@@ -13,6 +21,26 @@ export default class AddCarExperience extends LightningElement {
     }
 
     addExperience(){
+        const fields = {};
+        fields[NAME_FIELD.fieldApiName] = this.expTitle;
+        fields[EXPERIENCE_FIELD.fieldApiName] = this.expDescription;
+        fields[CAR_FIELD.fieldApiName] = this.carId;
+        
+        const recordInput = { apiName: EXPERIENCE_OBJECT.objectApiName, fields };
 
+        createRecord(recordInput).then( carExperience => {
+            this.showToast('SUCCESS', 'Experience record updated', 'success');
+        }).catch(error=>{
+            this.showToast('ERROR', error.body.message, 'error');
+        });
     }
+
+    showToast(title, message, variant) {
+      const evt = new ShowToastEvent({
+          title: title,
+          message: message,
+          variant: variant,
+      });
+      this.dispatchEvent(evt);
+  }
 }
