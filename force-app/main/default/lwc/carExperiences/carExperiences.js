11f4eb1b770621e97b0e8643c279e77b1129b898
Ahad Hossain <hossain1814@gmail.com>
2021-10-12 12:55:03 +0600

    S-13.cmt 25. fetch all experiences of a car from the server when the experience cmp is loaded.

--- force-app/main/default/lwc/carExperiences/carExperiences.js
+++ force-app/main/default/lwc/carExperiences/carExperiences.js
@@ -1,3 +1,30 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import getExperiences from '@salesforce/apex/CarExperience.getExperiences';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class CarExperiences extends LightningElement {}
+export default class CarExperiences extends LightningElement {
+ 
+    @api carId;
+    carExperience;
+
+    connectedCallback(){
+        this.getCarExperiences();
+    }
+
+    getCarExperiences(){
+        getExperiences({ carId: this.carId }).then((experiences)=> {
+            this.carExperience = experiences;
+        }).catch((error)=> {
+            this.showToast('ERROR', error.body.message, 'error');
+        })
+    }
+
+    showToast(title, message, variant) {
+      const evt = new ShowToastEvent({
+          title: title,
+          message: message,
+          variant: variant,
+      });
+      this.dispatchEvent(evt);
+    }
+}
