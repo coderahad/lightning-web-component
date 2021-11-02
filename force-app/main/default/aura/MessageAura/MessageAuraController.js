9b69a55282c0219a0edce02b567eaf97d87f6bf9
Ahad Hossain <hossain1814@gmail.com>
2021-11-02 22:32:04 +0600

    S-14.cmt-8. deleted all error producer. need to learn this topic from different source.

--- force-app/main/default/aura/MessageAura/MessageAuraController.js
+++ force-app/main/default/aura/MessageAura/MessageAuraController.js
@@ -1,15 +1,3 @@
 ({ 
-    sendHandler : function(component, event, helper) {},
-    messageHandler: function(component, event, helper) {
-        if(event && event.getParam("message")) {
-            const msg = event.getParam("message");
-            const messages = component.get("v.messages");
-            messages.push({
-                id: messages.length,
-                value: msg,
-                from: "LWC"
-            });
-            component.set("v.messages", messages);
-        }
-    }
+    sendHandler : function(component, event, helper) {}
 })
--- force-app/main/default/lwc/messageLwc/messageLwc.js
+++ force-app/main/default/lwc/messageLwc/messageLwc.js
@@ -1,11 +1,8 @@
 import { LightningElement, track, wire } from 'lwc';
-import messageDemo from "@salesforce/messageChannel/messageDemo__c";
-import { MessageContext, publish } from "lightning/messageService";
 
 export default class MessageLwc extends LightningElement {
     @track messages = []; // for arrray and object we need to use track decorator to track.
 
-    @wire(MessageContext) msgContext;
     sendHandler() {
         const inputElement = this.template.querySelector("lightning-input");
         if (inputElement) {
@@ -15,12 +12,7 @@ export default class MessageLwc extends LightningElement {
                 value: msg,
                 from: "LWC"
             });
-            // publish message
-            const messagePayload = {
-                message: msg
-            }
-
-            publish(this.msgContext, messageDemo, messagePayload);
+            // publish message            
             // after publishing input element become empty
             inputElement.value = "";
         }
