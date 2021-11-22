3ca737bd50f5d45a118af7cf8963868cdbf7e4b4
Ahad Hossain <hossain1814@gmail.com>
2021-11-22 19:32:14 +0600

    S-15.L-102. Making a callout to cowin api. get content-security Error. Error will be resolved in next section.

--- force-app/main/default/lwc/vaccineSlotFinder/vaccineSlotFinder.js
+++ force-app/main/default/lwc/vaccineSlotFinder/vaccineSlotFinder.js
@@ -3,7 +3,30 @@ import { LightningElement } from 'lwc';
 export default class VaccineSlotFinder extends LightningElement {
     centers = [];
     dates = [];
+
+    connectedCallback() {
+        this.fetchVaccineSlots();
+    }
+
+    async fetchVaccineSlots() {
+        // this is a http response
+        const vaccineSlotsRes = await fetch("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=302004&date=21-11-2021" //,
+           // We can provide request information here. As this is an open api we don't need to provide any header for this.
+            // { 
+            //     method: "POST",
+            //     header: {
+
+            //     }
+            // }
+        );
+        // get the json out of the http response
+        const slotsData = await vaccineSlotsRes .json();
+        console.log(slotsData);
+    }        
+
     get hideMessage(){
         return false;
     }
 }
+
+//https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=302004&date=21-11-2021
