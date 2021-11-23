80532ab0ffcb79fef764c69b03374ab6a7d4e81d
Ahad Hossain <hossain1814@gmail.com>
2021-11-23 13:58:37 +0600

    S-15.L-104. Build columns and rows data from fetched api data for rendering in lightning datatable.

--- force-app/main/default/lwc/vaccineSlotFinder/vaccineSlotFinder.js
+++ force-app/main/default/lwc/vaccineSlotFinder/vaccineSlotFinder.js
@@ -10,23 +10,38 @@ export default class VaccineSlotFinder extends LightningElement {
 
     async fetchVaccineSlots() {
         // this is a http response
-        const vaccineSlotsRes = await fetch("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=302004&date=21-11-2021" //,
-           // We can provide request information here. As this is an open api we don't need to provide any header for this.
-            // { 
-            //     method: "POST",
-            //     header: {
-
-            //     }
-            // }
-        );
+        const vaccineSlotsRes = await fetch("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=302004&date=21-11-2021");
         // get the json out of the http response
         const slotsData = await vaccineSlotsRes .json();
-        console.log(slotsData);
+        this.buildColumnsAndRows(slotsData.centers);
     }        
+
+    buildColumnsAndRows(data) {
+        // build columns/dates
+        const dates = new Map();
+        // as a key to this map we add name. 'name' also the very first column's fieldName.
+        dates.set("name", {label: "Center Name", fieldName: "name", type: "text"});
+
+        // build rows/centers
+        const centers = new Map();
+
+        for(const center of data) {
+            centers.set(center.center_id, {name: center.name})
+
+            for(const session of center.sessions) {
+                // destructuring syntax
+                const { date, available_capacity, min_age_limit } = session;
+
+                // add date as column in dates map
+                dates.set(date, {label: date, fieldName: date, type: "text"});
+            }
+        }
+
+        console.log(dates);
+        console.log(centers);
+    }
 
     get hideMessage(){
         return false;
     }
 }
-
-//https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=302004&date=21-11-2021
